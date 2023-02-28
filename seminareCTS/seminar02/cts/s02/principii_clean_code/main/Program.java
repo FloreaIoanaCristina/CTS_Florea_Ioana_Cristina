d2c8116ddd8c80a6baac044e024723e1339165e2
Ioana <Ioana@DESKTOP-VGKG330>
2023-02-28 13:21:03 +0200

    msg

---
+++ seminareCTS/seminar02/cts/s02/principii_clean_code/main/Program.java
@@ -0,0 +1,25 @@
+package cts.s02.principii_clean_code.main;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import cts.s02.principii_clean_code.clase.Angajat;
+import cts.s02.principii_clean_code.clase.Aplicant;
+import cts.s02.principii_clean_code.clase.reader.ReaderAngajat;
+
+public class Program {
+
+	public static void main(String[] args) {
+		ReaderAngajat readerAngajat = new ReaderAngajat();
+		List<Aplicant> listaAngajati;
+		try {
+			listaAngajati = readerAngajat.readAplicants("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
